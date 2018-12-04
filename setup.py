b1cc5c40671cf0abb26aaaf63afd42a9f640937b
Jan Jakubuv <jakubuv@gmail.com>
2018-12-04 22:43:19 +0100

    Include paramils sources

--- setup.py
+++ setup.py
@@ -7,6 +7,14 @@ setup(name='grackle',
       author='cbboyan',
       license='GPL3',
       packages=find_packages(),
-      scripts=['bin/fly-grackle.py','bin/grackle-wrapper.py'],
+      scripts=[
+         'bin/fly-grackle.py',
+         'bin/grackle-wrapper.py',
+         'paramils/param_ils_2_3_run.rb',
+         'paramils/algo_specifics.rb',
+         'paramils/global_helper.rb',
+         'paramils/param_reader.rb',
+         'paramils/stats_ils.rb'
+      ],
       zip_safe=False)
 
