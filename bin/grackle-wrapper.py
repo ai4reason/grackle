d1a587d956cc405f6da771536c6f074006105c8e
Jan Jakubuv <jakubuv@gmail.com>
2021-11-05 21:57:53 +0100

    Add __main__ test to grackle-wrapper

--- bin/grackle-wrapper.py
+++ bin/grackle-wrapper.py
@@ -1,25 +1,26 @@
 #!/usr/bin/env python3
 
-import sys
-import json
-import grackle
-
-RESULT="\nResult for ParamILS: %s, %s, %s, 1000000, %s"
-
-def run():
-   conf = json.loads(sys.argv[1])
-   cls = conf["cls"]
-   runner = grackle._load_class(cls)(config=conf)
-   runner.config["cutoff"] = int(float(sys.argv[4]))
-   inst = sys.argv[2]
-   seed = sys.argv[6]
-   params = runner.parse(sys.argv[7:])
-   (quality,clock) = runner.run(params, inst)[:2]
-
-   print(RESULT % ("OK", clock, quality, seed))
-
-if len(sys.argv) < 2:
-   print("usage: %s runner_config instance spec time cutoff seed arg1 val1 ..." % sys.argv[0])
-else:
-   run()
+if __name__ == '__main__':
+   import sys
+   import json
+   import grackle
+   
+   RESULT="\nResult for ParamILS: %s, %s, %s, 1000000, %s"
+   
+   def run():
+      conf = json.loads(sys.argv[1])
+      cls = conf["cls"]
+      runner = grackle._load_class(cls)(config=conf)
+      runner.config["cutoff"] = int(float(sys.argv[4]))
+      inst = sys.argv[2]
+      seed = sys.argv[6]
+      params = runner.parse(sys.argv[7:])
+      (quality,clock) = runner.run(params, inst)[:2]
+   
+      print(RESULT % ("OK", clock, quality, seed))
+   
+   if len(sys.argv) < 2:
+      print("usage: %s runner_config instance spec time cutoff seed arg1 val1 ..." % sys.argv[0])
+   else:
+      run()
 
